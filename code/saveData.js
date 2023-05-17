ac1362c7ec0418c4195eff3775fe2fd9c8a7b8b9
EricGriessbach <128109918+EricGriessbach@users.noreply.github.com>
2023-05-17 10:44:07 +0200

    Update saveData.js

--- code/saveData.js
+++ code/saveData.js
@@ -1,10 +1,10 @@
-    // Import the functions you need from the SDKs you need
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-    import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-    import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";    
-    // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    const firebaseConfig = {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";    
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
       apiKey: "AIzaSyDQYXw2WywxWOEB6rWqikpNobXO2YHVs6Y",
       authDomain: "onlineexperiment-a72fb.firebaseapp.com",
       projectId: "onlineexperiment-a72fb",
@@ -12,8 +12,8 @@
       messagingSenderId: "485166807071",
       appId: "1:485166807071:web:4b24c2813702bee8e8bd2a",
       measurementId: "G-8F4R7WCGH7"
-    };
-    
+};
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
