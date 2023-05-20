434568f56d7fa1f3351fe636f1a00e235f5ebf52
EricGriessbach <128109918+EricGriessbach@users.noreply.github.com>
2023-05-20 14:47:50 +0200

    Update pizza.js

--- scripts/pizza.js
+++ scripts/pizza.js
@@ -1,4 +1,4 @@
-//import { saveData, authenticate } from "./saveData.js";
+import { saveData, authenticate } from "./saveData.js";
 
 const gameContainer = document.getElementById("gameContainer");
 const toggleMappingButton = document.getElementById("toggleMapping");
