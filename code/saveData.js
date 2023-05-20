5025a39f9569ad7d6eab99a4af6847d4f0a202dc
EricGriessbach <128109918+EricGriessbach@users.noreply.github.com>
2023-05-20 13:54:58 +0200

    Update saveData.js

--- code/saveData.js
+++ code/saveData.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";    
@@ -23,6 +24,7 @@ const auth = getAuth(app);
 let uid = null;
 
 // Authenticate anonymously and set uid
+
 export async function authenticate() {
   try {
     const userCredential = await signInAnonymously(auth);
@@ -37,10 +39,10 @@ export async function authenticate() {
   }
 }
 
-export async function saveData(data) {
+export async function saveData(data, task) {
   if (uid) {
     try {
-      await setDoc(doc(db, "games", uid), data);
+      await setDoc(doc(db, task, uid), data);
       console.log("Document successfully written!");
     } catch (error) {
       console.error("Error writing document: ", error);
